60e30034e49e289acbcde37b079919cdf0b6cdf5
leozinho <79992686+leopompeu@users.noreply.github.com>
2022-06-18 02:33:33 -0300

    Add files via upload

---
+++ src/test/java/com/example/crmiv/CrmivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crmiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
